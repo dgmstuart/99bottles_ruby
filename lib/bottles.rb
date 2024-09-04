d086dd0a9d614d58bef3b62d97a5074746e9a6b8
Duncan Stuart <dgmstuart@gmail.com>
2024-09-04 22:19:18 +0200

    Refactor: remove nested conditional

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,19 +1,16 @@
 class Bottles
   def verse(initial_number)
-    if initial_number > 0
-      line1 = "#{n_bottles_of_beer(initial_number)} on the wall, #{n_bottles_of_beer(initial_number)}.\n"
-      new_number = initial_number - 1
-      line2 =
-        if new_number > 0
-          "Take one down and pass it around, #{n_bottles_of_beer(new_number)} on the wall.\n"
-        else
-          "Take it down and pass it around, no more bottles of beer on the wall.\n"
-        end
-      line1 + line2
-    else
-      "No more bottles of beer on the wall, no more bottles of beer.\n" \
-      "Go to the store and buy some more, #{n_bottles_of_beer(99)} on the wall.\n"
-    end
+    return empty_verse if initial_number == 0
+
+    line1 = "#{n_bottles_of_beer(initial_number)} on the wall, #{n_bottles_of_beer(initial_number)}.\n"
+    new_number = initial_number - 1
+    line2 =
+      if new_number > 0
+        "Take one down and pass it around, #{n_bottles_of_beer(new_number)} on the wall.\n"
+      else
+        "Take it down and pass it around, no more bottles of beer on the wall.\n"
+      end
+    line1 + line2
   end
 
   def verses(start_number, end_number)
@@ -32,6 +29,11 @@ class Bottles
   end
 
   private
+
+  def empty_verse
+    "No more bottles of beer on the wall, no more bottles of beer.\n" \
+    "Go to the store and buy some more, #{n_bottles_of_beer(99)} on the wall.\n"
+  end
 
   def n_bottles_of_beer(number)
     "#{number} bottle#{ number == 1 ? "" : "s"} of beer"
